b346bcc80250f6ee5ab1b4a9a469dea131f8c869
SilvaEng7 <rianalemeida11@gmail.com>
2026-03-26 00:34:59 -0300

    Fix conflitos

--- src/main/java/med/voll/api/dto/DadosCadastroPacientesDTO.java
+++ src/main/java/med/voll/api/dto/DadosCadastroPacientesDTO.java
@@ -1,38 +1,14 @@
 package med.voll.api.dto;
 
-<<<<<<< HEAD
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
-
-
 public record DadosCadastroPacientesDTO(
-    @NotBlank
-    String nome,
-
-    @NotBlank
-    @Email
-    String email,
-
-    @NotBlank
-    String telefone,
-
-    @NotBlank
-    String cpf,
-
-    @NotNull
-    @Valid
-    DadosCadastroEnderecoDTO endereco	
-=======
-public record DadosCadastroPacientesDTO(
-
-String nome,
-String email,
-String telefone,
-String cpf,
-DadosCadastroEnderecoDTO endereco	
->>>>>>> feature/listar-medico
-
-){}
+    @NotBlank String nome,
+    @NotBlank @Email String email,
+    @NotBlank String telefone,
+    @NotBlank String cpf,
+    @NotNull @Valid DadosCadastroEnderecoDTO endereco
+) {}
